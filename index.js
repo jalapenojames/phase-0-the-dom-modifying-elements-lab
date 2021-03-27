194f3a5a4f945f59cfb30678fb2e4e07618d9432
James <jaames.chen@gmail.com>
2021-03-27 12:07:35 -0400

    Completed Assignment (modifying dom elements)

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+
+document.getElementById("main").remove();
+
+// let body = document.querySelector('body');
+// body.append('h1');
+// document.querySelector('h1').id = 'victory';
+
+// let newHeader = document.querySelector('h1');
+
+const element = document.createElement('h1');
+let newHeader = document.body.appendChild(element);
+document.querySelector('h1').id = 'victory';
+
+document.querySelector('h1').innerHTML = 'James is the champion';
